694f7d0b50bea48a310f1429749f05ba9652ca68
Agnes <agsawicka@gmail.com>
2022-09-26 10:15:21 -0400

    auth

--- app/models/logistics_manager.rb
+++ app/models/logistics_manager.rb
@@ -1,5 +1,5 @@
 class LogisticsManager < ApplicationRecord
-    has_many :deliveries, through: :user
+    # has_many :deliveries, through: :user
     has_many :constructions
     has_many :equipments, through: :constructions
     has_many :contractors, through: :constructions
