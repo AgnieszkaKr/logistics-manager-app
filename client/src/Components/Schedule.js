82b4dc87b463422623854f4cc4923dfc5a73164c
Agnes <agsawicka@gmail.com>
2022-10-19 12:16:00 -0400

    scheduler create

--- client/src/Components/Schedule.js
+++ client/src/Components/Schedule.js
@@ -4,7 +4,7 @@ import './Styling/Schedule.css'
 import { useLocation, Link } from 'react-router-dom'
 import type { ProcessedEvent, SchedulerHelpers} from "@aldabil/react-scheduler/types";
 import { TextField, Button, DialogActions } from "@mui/material";
-
+import CustomEditor from './CustomEditor.js'
 
 function Schedule() {
     const[equipment, setEquipment] = useState([{id: "loading...", name: "loading...", deliveries: "loading..."}])
@@ -39,7 +39,7 @@ function Schedule() {
    
     // Create new delivery
     const handleConfirm = (event, action) => {
-        console.log(event)
+        console.log(event, action)
         if (action === "edit") {
         /** PUT event to remote DB */
             console.log({
@@ -81,7 +81,7 @@ function Schedule() {
                     end: new Date(event.end)})
                 })
                 return {
-                    event_id: Math.floor(Math.random() * 10),
+                    event_id: event.id,
                     title: event.title,
                     start: new Date(event.start),
                     end: new Date(event.end)}
@@ -109,7 +109,15 @@ function Schedule() {
                                 } ))
     console.log(deliveryTransformed)
 
-
+    const updatedEvent = (time, updated) =>{
+        console.log("time", time, "updated", updated)
+        // return{
+        //             event_id: event.id,
+        //             title: event.title,
+        //             start: new Date(event.start),
+        //             end: new Date(event.end)}
+        }
+    
 
 
     return (
@@ -153,6 +161,28 @@ function Schedule() {
                         events={deliveryTransformed}
                         onConfirm={handleConfirm}
                         onDelete={handleDelete}
+                        // onEventDrop={updatedEvent}
+                        month={{
+                              weekDays: [0, 1, 2, 3, 4, 5, 6],
+                              weekStartOn: 1,
+                              startHour: 6,
+                              endHour: 24,
+                              step: 60,
+                            }}
+                        day={{
+                          startHour: 6,
+                          endHour: 14,
+                          step: 60,
+                         
+                        }}
+                        week={{
+                            weekDays: [0, 1, 2, 3, 4, 5, 6],
+                            weekStartOn: 1,
+                            startHour: 5,
+                            endHour: 24,
+                            step: 60,
+
+                        }}
                     />
             
             </div>):(null))}
@@ -161,3 +191,208 @@ function Schedule() {
 }
 
 export default Schedule
+
+
+
+
+
+
+      // dialogMaxWidth="sm"
+      // loading={loading}
+      // view="month"
+      // editable={false}
+      // deletable={false}
+      // selectedDate={new Date()}
+      // height={800}
+      // week={{
+      //   weekDays: [0, 1, 2, 3, 4, 5],
+      //   weekStartOn: 6,
+      //   startHour: 7,
+      //   endHour: 22,
+      //   step: 60,
+      //   cellRenderer: () => {
+      //     return <>week</>;
+      //   },
+      // }}
+      // 
+      // day={{
+      //   startHour: 6,
+      //   endHour: 14,
+      //   step: 60,
+      //   cellRenderer: () => {
+      //     return <h1>DAY</h1>;
+      //   },
+      // }}
+      // day={{
+      //   startHour: 8,
+      //   endHour: 18,
+      //   step: 20,
+      // }}
+      // remoteEvents={async (query) => {
+      //   await new Promise((res, rej) => {
+      //     setTimeout(() => {
+      //       // setEvents(EVENTS);
+      //       res("");
+      //     }, 1000);
+      //   });
+      //   // return null;
+      //   // return EVENTS;
+      // }}
+      // resources={[
+      // {
+      //   admin_id: 1,
+      //   title: "One",
+      //   mobile: "555666777",
+      //   avatar: "https://picsum.photos/200/300",
+      //   color: "#ab2d2d",
+      // },
+      // {
+      //   admin_id: 2,
+      //   title: "Two",
+      //   mobile: "555666777",
+      //   avatar: "https://picsum.photos/200/300",
+      //   color: "#58ab2d",
+      // },
+      //   {
+      //     admin_id: 3,
+      //     title: "Three",
+      //     mobile: "555666777",
+      //     avatar: "https://picsum.photos/200/300",
+      //     color: "#a001a2",
+      //   },
+      //   {
+      //     admin_id: 4,
+      //     title: "Four",
+      //     mobile: "555666777",
+      //     avatar: "https://picsum.photos/200/300",
+      //     color: "#08c5bd",
+      //   },
+      // ]}
+      // resourceFields={{
+      //   idField: "admin_id",
+      //   textField: "title",
+      //   subTextField: "mobile",
+      //   avatarField: "title",
+      //   colorField: "color",
+      // }}
+      // resourceViewMode="tabs"
+      // recourseHeaderComponent={(recourse) => {
+      //   console.log(recourse);
+      //   return <div>HAHA</div>;
+      // }}
+      // fields={[
+      //   {
+      //     name: "description",
+      //     type: "input",
+      //     config: { label: "Description", multiline: true, rows: 4 },
+      //   },
+      //   {
+      //     name: "admin_id",
+      //     type: "select",
+      //     config: { label: "Assignee", required: true, multiple: "chips" },
+      //     // default: [1, 2],
+      //     options: [
+      //       // {
+      //       //   id: 1,
+      //       //   text: "One",
+      //       //   value: 1,
+      //       // },
+      //       {
+      //         id: 2,
+      //         text: "Two",
+      //         value: 2,
+      //       },
+      //       {
+      //         id: 3,
+      //         text: "Three",
+      //         value: 3,
+      //       },
+      //       {
+      //         id: 4,
+      //         text: "Four",
+      //         value: 4,
+      //       },
+      //     ],
+      //   },
+      // ]}
+      // onConfirm={async (event, action) => {
+      //   console.log(action);
+      //   return new Promise((res, rej) => {
+      //     setTimeout(() => {
+      //       res({
+      //         ...event,
+      //         event_id: event.event_id || Math.random(),
+      //         // title: "From Custom",
+      //         // start: new Date(new Date().setHours(11)),
+      //         // end: new Date(new Date().setHours(18)),
+      //       });
+      //     }, 1000);
+      //   });
+      // }}
+      // onDelete={async (id) => {
+      //   await new Promise((res, rej) => {
+      //     setTimeout(() => {
+      //       setEvents((prev) => {
+      //         return prev.filter((p) => p.event_id !== id);
+      //       });
+      //       res("");
+      //     }, 1000);
+      //   });
+      // }}
+      // customEditor={(scheduler) => <CustomEditor scheduler={scheduler} />}
+      // viewerExtraComponent={(fields, e) => {
+      //   return (
+      //     <div>
+      //       {Array.from("a".repeat(50)).map((a, i) => (
+      //         <div key={i}>Extra</div>
+      //       ))}
+      //     </div>
+      //   );
+      //   // console.log(fields, e);
+      //   // return (
+      //   //   <div>
+      //   //     {fields.map((a, i) => (
+      //   //       <div key={i}>{e.description}</div>
+      //   //     ))}
+      //   //   </div>
+      //   // );
+      // }}
+      // viewerTitleComponent={(event) => <>{event.title}</>}
+      // direction="rtl"
+      //  locale={ptBR}
+      //  hourFormat={"24"}
+      //  translations={{
+      //   navigation: {
+      //     month: "Mês",
+      //     week: "Semana",
+      //     day: "Dia",
+      //     today: "Hoje"
+      //   },
+      //   form: {
+      //     addTitle: "Novo Evento",
+      //     editTitle: "Editar Evento",
+      //     confirm: "Confirmar",
+      //     delete: "Excluir",
+      //     cancel: "Cancelar",
+      //   },
+      //   event: {
+      //     title: "Título",
+      //     start: "Início",
+      //     end: "Fim"
+      //   },
+      //   moreEvents: "mais..."
+      // }}
+      // onEventDrop={async (time, updated) => {
+      //   return new Promise((res) => {
+      //     setTimeout(() => {
+      //       setEvents((prev: any) => {
+      //         return prev.map((e) =>
+      //           e.event_id === updated.event_id ? updated : e
+      //         );
+      //       });
+      //       res();
+      //     }, 1000);
+      //   });
+      // }}
+
+
