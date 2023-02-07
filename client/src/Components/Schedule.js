ddfe4610ab00ccc98fd36bea58a36b64909d7d8c
Agnes <agsawicka@gmail.com>
2023-02-07 15:30:40 -0500

    works

--- client/src/Components/Schedule.js
+++ client/src/Components/Schedule.js
@@ -24,144 +24,144 @@ function Schedule() {
     const[contractor, setContractor]=useState(false)
     // send request for all schedules, loop over and generate buttons
     // when  click on the button, return false to any other gates and true to choosen gate
-//     const location = useLocation()
-//     // site id:
-//     const {id , name} = location.state
-//     useEffect(() =>{
-//         fetch(`/equipment/site/${id}`)
-//         .then(res =>{
-//         if(res.ok){
-//             res.json()
-//             .then(e => setEquipment(e));
-//         } else {
-//             res.json().then(e => setErrors(e.errors))}
-//         })
+    const location = useLocation()
+    // site id:
+    const {id , name} = location.state
+    useEffect(() =>{
+        fetch(`/equipment/site/${id}`)
+        .then(res =>{
+        if(res.ok){
+            res.json()
+            .then(e => setEquipment(e));
+        } else {
+            res.json().then(e => setErrors(e.errors))}
+        })
 
-//     },[])
-
-
-//     const[newDeliveryRes, setNewDeliveryRes]=useState()
+    },[equipment])
 
 
-//     const handleConfirm = (event, action) => {
-//         if (action === "edit") {
-//         // update event in state, 
-//          deliveryTransformed = deliveryTransformed.map(delivery => {
-//             if( delivery.event_id === event.event_id){
+    const[newDeliveryRes, setNewDeliveryRes]=useState()
 
-//                 delivery.title = event.title
-//                 delivery.start = event.start
-//                 delivery.end = event.end
-//                 return delivery
-//             }else {
-//                 return delivery
-//             }
-//         })
+
+    const handleConfirm = (event, action) => {
+        if (action === "edit") {
+        // update event in state, 
+         deliveryTransformed = deliveryTransformed.map(delivery => {
+            if( delivery.event_id === event.event_id){
+
+                delivery.title = event.title
+                delivery.start = event.start
+                delivery.end = event.end
+                return delivery
+            }else {
+                return delivery
+            }
+        })
            
-//         // send fetch request to update on server
-//         fetch(`/deliveries/${event.event_id}`,{
-//                 method:"PATCH",
-//                 headers:{ 'Content-Type':'application/json'},
-//                 body: JSON.stringify({
-//                     finish_time: event.end,
-//                     start_time: event.start,
-//                     title: event.title,
-//                 })})
-//                 .then(res =>{
-//                     if(res.ok){
-//                         res.json().then(console.log);
-//                     } else {
-//                         res.json().then(e =>console.log(e.errors))
-//                     }})
-//         return{
-//             event_id: event.id,
-//             title: event.title,
-//             start: event.start,
-//             end: event.end
-//             }, deliveryTransformed
+        // send fetch request to update on server
+        fetch(`/deliveries/${event.event_id}`,{
+                method:"PATCH",
+                headers:{ 'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    finish_time: event.end,
+                    start_time: event.start,
+                    title: event.title,
+                })})
+                .then(res =>{
+                    if(res.ok){
+                        res.json().then(console.log);
+                    } else {
+                        res.json().then(e =>console.log(e.errors))
+                    }})
+        return{
+            event_id: event.id,
+            title: event.title,
+            start: event.start,
+            end: event.end
+            }, deliveryTransformed
 
-//         } 
-// //CREATE NEW MESSAGE 
-//         if (action === "create") {
-//         /**POST event to remote DB */
+        } 
+//CREATE NEW MESSAGE 
+        if (action === "create") {
+        /**POST event to remote DB */
             
-//             let newDelivery = {
-//                 equipment_id: openSchedule[0],
-//                 title: event.title,
-//                 start_time: event.start,
-//                 finish_time: event.end }
-//                 console.log(newDelivery)
-//             fetch('/deliveries',{
-//                 method:"POST",
-//                 headers:{ 'Content-Type':'application/json'},
-//                 body: JSON.stringify(newDelivery)})
-//                 .then(res =>{
-//                     console.log(res)
-//                     if(res.ok){
-//                         res.json().then(console.log(res));
-//                     } else {
-//                         res.json().then(e =>console.log(e.errors))
-//                     }
-//                 })
-//                 return {
-//                     event_id: event.id,
-//                     title: event.title,
-//                     start: new Date(event.start),
-//                     end: new Date(event.end)}
-//             }
-//     }
+            let newDelivery = {
+                equipment_id: openSchedule[0],
+                title: event.title,
+                start_time: event.start,
+                finish_time: event.end }
+                console.log(newDelivery)
+            fetch('/deliveries',{
+                method:"POST",
+                headers:{ 'Content-Type':'application/json'},
+                body: JSON.stringify(newDelivery)})
+                .then(res =>{
+                    console.log(res)
+                    if(res.ok){
+                        res.json().then(console.log(res));
+                    } else {
+                        res.json().then(e =>console.log(e.errors))
+                    }
+                })
+                return {
+                    event_id: event.id,
+                    title: event.title,
+                    start: new Date(event.start),
+                    end: new Date(event.end)}
+            }
+    }
 
-//     const handleDelete = (deletedId) => {
-//         fetch(`/deliveries/${deletedId}`,{
-//         method:'DELETE',
-//         headers:{
-//         'Content-Type':'application/json'
-//         } })
-//         .then(req => req.json())
-//         return deletedId
-//     }
-//     const [confirmedDeliveries, setConfirmDeliveries] =useState({})
-//     let deliveryTransformed = deliveries.map(d=>  ({
-//                                     event_id: d.id,
-//                                     title: d.title,
-//                                     start: new Date(d.start_time),
-//                                     end: new Date(d.finish_time),
-//                                     color: "green",
-//                                     key: 1
-//                                     // disabled: true
-//                                 } ))
-//     // update/ drop works
-//     const updatedEvent = (time, updated) =>{
-//         let id = updated.event_id
-//         console.log("edit")
-//         // change deliveryTransformed and return it at the end
-//         deliveryTransformed = deliveryTransformed.map(delivery => {
-//             if( delivery.event_id === updated.event_id){
-//                 delivery.start = updated.start
-//                 delivery.end = updated.end
-//                 return delivery
-//             }else {
-//                 return delivery
-//             }
-//         })
+    const handleDelete = (deletedId) => {
+        fetch(`/deliveries/${deletedId}`,{
+        method:'DELETE',
+        headers:{
+        'Content-Type':'application/json'
+        } })
+        .then(req => req.json())
+        return deletedId
+    }
+    const [confirmedDeliveries, setConfirmDeliveries] =useState({})
+    let deliveryTransformed = deliveries.map(d=>  ({
+                                    event_id: d.id,
+                                    title: d.title,
+                                    start: new Date(d.start_time),
+                                    end: new Date(d.finish_time),
+                                    color: "green",
+                                    key: 1
+                                    // disabled: true
+                                } ))
+    // update/ drop works
+    const updatedEvent = (time, updated) =>{
+        let id = updated.event_id
+        console.log("edit")
+        // change deliveryTransformed and return it at the end
+        deliveryTransformed = deliveryTransformed.map(delivery => {
+            if( delivery.event_id === updated.event_id){
+                delivery.start = updated.start
+                delivery.end = updated.end
+                return delivery
+            }else {
+                return delivery
+            }
+        })
 
-//         // send fetch request patch 
-//         fetch(`/deliveries/${id}`,{
-//                 method:"PATCH",
-//                 headers:{ 'Content-Type':'application/json'},
-//                 body: JSON.stringify({
-//                     finish_time: updated.end,
-//                     start_time: updated.start
-//                 })})
-//                 .then(res =>{
-//                     if(res.ok){
-//                         res.json().then(console.log);
-//                     } else {
-//                         res.json().then(e =>console.log(e.errors))
-//                     }})
+        // send fetch request patch 
+        fetch(`/deliveries/${id}`,{
+                method:"PATCH",
+                headers:{ 'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    finish_time: updated.end,
+                    start_time: updated.start
+                })})
+                .then(res =>{
+                    if(res.ok){
+                        res.json().then(console.log);
+                    } else {
+                        res.json().then(e =>console.log(e.errors))
+                    }})
 
-//      return deliveryTransformed
-//     }
+     return deliveryTransformed
+    }
     
 
 
@@ -171,7 +171,7 @@ function Schedule() {
             <ul class="nav nav-pills mb-3"  role="tablist">
                 <li class="nav-item dropdown">
                     <button className="nav-link" id="" aria-current="page" data-bs-toggle="dropdown" data-bs-target="" aria-selected="true" onClick={() => {setDisplayLayout(false); setDisplaySchedule(true); setUpdateContractors(false); setEditEquipment(false); setAddContractor(false) ; setEditEquipment(false); setPendingInvitations(false) }} >SCHEDULE </button>
-                    {/* <ul className="dropdown-menu">
+                    <ul className="dropdown-menu">
                         {equipment.map(element => <li ><div className="dropdown-item"   onClick={() => {
                             setDeliveries(element.deliveries)
                             let deliveryTransformed = deliveries.map(d=>  ({
@@ -184,7 +184,7 @@ function Schedule() {
                             setOpenSchedule([element.id, element.name]); 
                             setConfirmDeliveries(element.deliveries) }}>{element.name}</div></li>)}
                                 
-                    </ul> */}
+                    </ul>
                 </li>           
                 <li className="nav-item">
                     <button className="nav-link" id="pills-contact-tab"   type="button"   aria-selected="false" onClick={() => {setDisplayLayout(true) }}>Layout</button>
@@ -204,7 +204,7 @@ function Schedule() {
                 
             </ul>
             </div>
-            {/* {displayLayout ? (
+            {displayLayout ? (
                 <Layout site_id={id} setDisplayLayout={setDisplayLayout} /> 
             ): null}
             {pendingInvitations ? (
@@ -257,7 +257,7 @@ function Schedule() {
                 </div>):null)} 
             </>)
             : null }
-                 */}
+                
 
 
 
